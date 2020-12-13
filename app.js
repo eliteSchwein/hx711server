68307e3c62dfd5612a3abd3c6fe51e71fa3694a6
eliteSCHW31N <39838619+eliteSchwein@users.noreply.github.com>
2020-12-13 17:46:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var cors = require('cors');
+const hx711 = require('/hx711');
 
 const PORT = 8081;
 
 var scalevalue1 = 1500;
 var scalevalue2 = 0;
 
-const sensor1 = new HX711(21, 20);
-const sensor2 = new HX711(26, 19);
+const sensor1 = new hx711(21, 20);
+const sensor2 = new hx711(26, 19);
 var app = express();
 app.use(cors());
 
