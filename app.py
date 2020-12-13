ba2a0831c3cf2df74516292ccbd3435f9289c957
eliteSCHW31N <business@eliteschw31n.de>
2020-12-13 20:18:55 +0100

    add website

--- app.py
+++ app.py
@@ -1,17 +1,84 @@
 #!/usr/bin/python3
-    from hx711 import HX711
+   
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+import sys
+
+hostName = "localhost"
+serverPort = 8081
+
+referenceUnit = 1
+
+import RPi.GPIO as GPIO
+from hx711 import HX711
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
 
     try:
-        hx711 = HX711(
-            dout_pin=5,
-            pd_sck_pin=6,
-            channel='A',
-            gain=64
-        )
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
 
-        hx711.reset()   # Before we start, reset the HX711 (not obligate)
-        measures = hx711.get_raw_data(num_measures=3)
-    finally:
-        GPIO.cleanup()  # always do a GPIO cleanup in your scripts!
+    webServer.server_close()
+    print("Server stopped.")
 
-    print("\n".join(measures))
+def cleanAndExit():
+    print("Cleaning...")
+    GPIO.cleanup()
+        
+    print("Bye!")
+    sys.exit()
+
+hx1 = HX711(20, 21)
+hx2 = HX711(19, 26)
+
+hx1.set_reading_format("MSB", "MSB")
+hx2.set_reading_format("MSB", "MSB")
+
+hx1.set_reference_unit(referenceUnit)
+hx2.set_reference_unit(referenceUnit)
+
+hx1.reset()
+hx2.reset()
+
+print("Setup done")
+
+while True:
+    try:
+        # These three lines are usefull to debug wether to use MSB or LSB in the reading formats
+        # for the first parameter of "hx.set_reading_format("LSB", "MSB")".
+        # Comment the two lines "val = hx.get_weight(5)" and "print val" and uncomment these three lines to see what it prints.
+        
+        # np_arr8_string = hx.get_np_arr8_string()
+        # binary_string = hx.get_binary_string()
+        # print binary_string + " " + np_arr8_string
+        
+        # Prints the weight. Comment if you're debbuging the MSB and LSB issue.
+        val1 = hx1.get_weight(5)
+        print(val1)
+
+        # To get weight from both channels (if you have load cells hooked up 
+        # to both channel A and B), do something like this
+        #val_A = hx.get_weight_A(5)
+        #val_B = hx.get_weight_B(5)
+        #print "A: %s  B: %s" % ( val_A, val_B )
+
+        hx1.power_down()
+        hx1.power_up()
+        time.sleep(0.1)
+
+    except (KeyboardInterrupt, SystemExit):
+        cleanAndExit()
