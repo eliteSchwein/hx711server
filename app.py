0e89847429cc7a21d1a6705a014d10fe0f01312e
eliteSCHW31N <business@eliteschw31n.de>
2020-12-13 23:30:28 +0100

    test multi threading

--- app.py
+++ app.py
@@ -4,11 +4,41 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 import sys
 import threading
+import RPi.GPIO as GPIO
+from hx711 import HX711
 
 referenceUnit = 1
 
-import RPi.GPIO as GPIO
-from hx711 import HX711
+serverPort = 8081
+
+val1 = 0
+val2 = 0
+
+def runScale1():
+    hx1 = HX711(20, 21)
+    hx1.set_reading_format("MSB", "MSB")
+    hx1.set_reference_unit(referenceUnit)
+    hx1.reset()
+    while True:
+        try:
+            val1 = hx1.get_weight(5)
+
+            hx1.power_down()
+            hx1.power_up()
+            time.sleep(0.5)
+
+def runScale2():
+    hx2 = HX711(19, 26)
+    hx2.set_reading_format("MSB", "MSB")
+    hx2.set_reference_unit(referenceUnit)
+    hx2.reset()
+    while True:
+        try:
+            val2 = hx2.get_weight(5)
+
+            hx2.power_down()
+            hx2.power_up()
+            time.sleep(0.5)
 
 def cleanAndExit():
     print("Cleaning...")
@@ -26,24 +56,16 @@ GPIO.setwarnings(False)
 
 print("Setting Scales up!")
 
-hx1 = HX711(20, 21)
-hx2 = HX711(19, 26)
+hx1thread = threading.Thread(target=runScale1,args=())
+hx1thread.daemon
+hx1thread.start()
 
-hx1.set_reading_format("MSB", "MSB")
-hx2.set_reading_format("MSB", "MSB")
-
-hx1.set_reference_unit(referenceUnit)
-hx2.set_reference_unit(referenceUnit)
-
-hx1.reset()
-hx2.reset()
+hx2thread = threading.Thread(target=runScale2,args=())
+hx2thread.daemon
+hx2thread.start()
 
 print("Scale Startup done.")
 
-val1 = 0
-val2 = 0
-
-serverPort = 8081
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
