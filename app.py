0dfd77b142bf7866afadf1dc7850694eeb46f452
eliteSCHW31N <business@eliteschw31n.de>
2020-12-13 20:52:47 +0100

    reset file

--- app.py
+++ app.py
@@ -1,49 +1,55 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
    
-import SimpleHTTPServer
-import SocketServer
+import time
+import sys
 
-serverPort = 8081
+EMULATE_HX711=False
 
 referenceUnit = 1
 
-import RPi.GPIO as GPIO
-from hx711 import HX711
-
-appname = "App1"
-
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-
-httpd = SocketServer.TCPServer(("", PORT), Handler)
-
-print "serving at port", PORT
-httpd.serve_forever()
-
-body = '<div class=cell button>'
-body += ' <img src=./icons/NE.png onclick=image(this) />'
-body += '<h3> NE</h3>'
-body += '</div>'
+if not EMULATE_HX711:
+    import RPi.GPIO as GPIO
+    from hx711 import HX711
+else:
+    from emulated_hx711 import HX711
 
 def cleanAndExit():
     print("Cleaning...")
-    GPIO.cleanup()
+
+    if not EMULATE_HX711:
+        GPIO.cleanup()
         
     print("Bye!")
     sys.exit()
 
-hx1 = HX711(20, 21)
-hx2 = HX711(19, 26)
+hx = HX711(20, 21)
 
-hx1.set_reading_format("MSB", "MSB")
-hx2.set_reading_format("MSB", "MSB")
+# I've found out that, for some reason, the order of the bytes is not always the same between versions of python, numpy and the hx711 itself.
+# Still need to figure out why does it change.
+# If you're experiencing super random values, change these values to MSB or LSB until to get more stable values.
+# There is some code below to debug and log the order of the bits and the bytes.
+# The first parameter is the order in which the bytes are used to build the "long" value.
+# The second paramter is the order of the bits inside each byte.
+# According to the HX711 Datasheet, the second parameter is MSB so you shouldn't need to modify it.
+hx.set_reading_format("MSB", "MSB")
 
-hx1.set_reference_unit(referenceUnit)
-hx2.set_reference_unit(referenceUnit)
+# HOW TO CALCULATE THE REFFERENCE UNIT
+# To set the reference unit to 1. Put 1kg on your sensor or anything you have and know exactly how much it weights.
+# In this case, 92 is 1 gram because, with 1 as a reference unit I got numbers near 0 without any weight
+# and I got numbers around 184000 when I added 2kg. So, according to the rule of thirds:
+# If 2000 grams is 184000 then 1000 grams is 184000 / 2000 = 92.
+#hx.set_reference_unit(113)
+hx.set_reference_unit(referenceUnit)
 
-hx1.reset()
-hx2.reset()
+hx.reset()
 
-print("Setup done")
+hx.tare()
+
+print("Tare done! Add weight now...")
+
+# to use both channels, you'll need to tare them both
+#hx.tare_A()
+#hx.tare_B()
 
 while True:
     try:
@@ -56,8 +62,8 @@ while True:
         # print binary_string + " " + np_arr8_string
         
         # Prints the weight. Comment if you're debbuging the MSB and LSB issue.
-        val1 = hx1.get_weight(5)
-        print(val1)
+        val = hx.get_weight(5)
+        print(val)
 
         # To get weight from both channels (if you have load cells hooked up 
         # to both channel A and B), do something like this
@@ -65,8 +71,8 @@ while True:
         #val_B = hx.get_weight_B(5)
         #print "A: %s  B: %s" % ( val_A, val_B )
 
-        hx1.power_down()
-        hx1.power_up()
+        hx.power_down()
+        hx.power_up()
         time.sleep(0.1)
 
     except (KeyboardInterrupt, SystemExit):
